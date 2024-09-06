2e62774a49aed286a24d3bc153c607f8ed8458ff
chambrin <achambrin@outlook.fr>
2024-09-06 16:46:43 +0200

    💄 isFirst variant

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,62 +11,49 @@ import ManImg from "@public/images/man.jpg";
 export default function Home() {
     return (
         <main>
-            <Content
-                title={contentPageData[0].title}
-                content={contentPageData[0].content}
-                imageSrc={contentPageData[0].imageSrc}
-                imagePosition="right"
-            />
-            <Carousel />
-            <Content
-                title={contentPageData[1].title}
-                content={contentPageData[1].content}
-                imageSrc={contentPageData[1].imageSrc}
-                imagePosition="left"
-            />
-            <Carousel />
-            <Content
-                title={contentPageData[2].title}
-                content={contentPageData[2].content}
-                imageSrc={contentPageData[2].imageSrc}
-                imagePosition="right"
-            />
-            <Carousel />
-            <Content
-                title={contentPageData[3].title}
-                content={contentPageData[3].content}
-                imageSrc={contentPageData[3].imageSrc}
-                imagePosition="left"
-            />
+            {contentPageData.map((section, index) => (
+                <React.Fragment key={index}>
+                    <Content
+                        title={section.title}
+                        content={section.content}
+                        imageSrc={section.imageSrc}
+                        imagePosition={section.imagePosition}
+                        isFirst={section.isFirst}
+                    />
+                    <Carousel />
+                </React.Fragment>
+            ))}
         </main>
     );
 }
-
 // Page data
 const contentPageData = [
     {
-        title: 'Home coffee',
-        content: 'Got a fancy coffee machine sitting at home, but struggling with that perfect brew? We’ve created an in-person 2 hour session to learn all things home brewing. From machine set up, pouring basics and perfecting your milk. Ready to make delicious coffee at home?',
+        title: 'For Coffee Drinkers',
+        content: '',
         imageSrc: LandingImg,
-        imagePosition: 'right'
+        imagePosition: 'right',
+        isFirst: true
     },
     {
         title: 'Home coffee',
         content: 'Got a fancy coffee machine sitting at home, but struggling with that perfect brew? We’ve created an in-person 2 hour session to learn all things home brewing. From machine set up, pouring basics and perfecting your milk. Ready to make delicious coffee at home?',
         imageSrc: EspressoImg,
-        imagePosition: 'left'
+        imagePosition: 'left',
+        isFirst: false
     },
     {
         title: 'Coffee subscriptions',
         content: 'Our customisable coffee subscription allows you to select bag size, grind setting, frequency, and duration…so you never run out of coffee again.',
         imageSrc: BoxImg,
-        imagePosition: 'right'
+        imagePosition: 'right',
+        isFirst: false
     },
     {
         title: 'Have a cafe? Or just need lots of coffee?',
         content: 'We\'ve got you. Redbrick is here to help you serve delicious coffee. We work in a collaborative way to find the best approach to coffee for your business.',
         imageSrc: ManImg,
-        imagePosition: 'right'
+        imagePosition: 'left',
+        isFirst: false
     }
 ];
-
--- src/components/feature/content/ContentView.tsx
+++ src/components/feature/content/ContentView.tsx
@@ -3,13 +3,14 @@ import Image, { StaticImageData } from "next/image";
 import { editorial } from "@/lib/font";
 
 interface ContentProps {
-    imagePosition?: 'left' | 'right';
+    imagePosition: 'left' | 'right';
     title: string;
     content: string;
     imageSrc: string | StaticImageData;
+    isFirst?: boolean;
 }
 
-export default function Content({ imagePosition = 'left', title, content, imageSrc }: ContentProps) {
+export default function Content({ imagePosition = 'left', title, content, imageSrc, isFirst = false }: ContentProps) {
     const isImageLeft = imagePosition === 'left';
 
     return (
@@ -24,8 +25,10 @@ export default function Content({ imagePosition = 'left', title, content, imageS
                 />
             </div>
             <div className={`w-full md:w-1/2 flex flex-col justify-center p-8 ${isImageLeft ? 'md:p-32' : 'md:pl-0 md:pr-32' } order-1 md:order-none`}>
-                <h2 className={`text-4xl md:text-8xl mb-2 ${editorial.className}`}>{title}</h2>
-                <p className="text-lg md:text-xl mt-4 md:mt-9">{content}</p>
+                <h2 className={`mb-2 ${editorial.className} ${isFirst ? 'text-7xl md:text-9xl ' : 'text-4xl md:text-8xl'}`}>
+                    {title}
+                </h2>
+                {!isFirst && <p className="text-lg md:text-xl mt-4 md:mt-9">{content}</p>}
             </div>
         </div>
     );
